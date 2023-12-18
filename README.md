9088a3ce4cc150747078c303b69efb0a8fa9545f
YICHUNLIN <yichunb03@gmail.com>
2023-12-18 16:24:48 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # CameraUtil
-# CameraUtil
+
+> for raspberry pi, and camera interval capture.
+
+## Main usage
+
+[libcamera](https://www.npmjs.com/package/libcamera)
+
+## methods
+
+### constructor
+
+> params
+
+1. option: Json Object
+	- output: STRING	| path of output path
+	- delaySecond: INT	| time of interval delay time
+	- type: STRING		| type of cap method
+2. onCapSucc: callback function, when cap successed
+3. onCapErr: callback function, when cap error
+
+### internal methods
+
+> __video
+
+> __photo
+
+### public methods
+
+> start
+
+Starting interval by "delaySecond", and select by "type".
+
+
+> end
+
+ending interval
+
+
---
+++ index.js
@@ -0,0 +1,72 @@
+
+const { libcamera } = require('libcamera');
+
+/*
+ * @description 相機
+ * @params
+ * 	- option: JsonObject
+ * 		- {
+ *			output: STRING		| path of output file
+ *			delaySecond: INT	| time of interval delay
+ *			type: STRING		| type of cap method
+ * 		  }
+ * 	- onCapSucc: call back function, when cap success
+ * 	- onCapErr: call back function, when cap error
+ * */
+function Camera(option, onCapSucc, onCapErr){
+	this.intervalID = undefined;
+	this.output = option.output;
+	this.delaySecond = option.delaySecond;
+	this.type = option.type;
+	this.onCapSucc = onCapzSucc;
+	this.onCapErr = onCapErr;
+}
+
+/*
+ * @description photo method
+ * @method libcamera.still
+ * */
+Camera.prototype.__photo == function(){
+	const tthis = this;
+	return libcamera.still({ config: { output: tthis.output } });
+}
+
+/*
+ * @description video method
+ * @method libcamera.vid
+ * */
+Camera.prototype.__video = function(){
+	const tthis = this;
+	return libcamera.vid({ config: {output: tthis.output}});
+}
+
+/*
+ * @description startting camera interval
+ *
+ * */
+Camera.prototype.start = function(){
+	const tthis = this;
+	let op = null;
+	if (this.type == "vodeo") op = tthis.__video;
+	else if (this.type == "photo") op = tthis.__photo;
+	if (!op) return {result: false, message: `camera start error, op = null`};
+	this.intervalID = setInterval(() => {
+		op
+		.then(result => tthis.onCapSucc(result))
+		.catch(err => tthis.onCapzErr(err));
+	}, this.delaySecond * 1000);
+	return {result: true, message: `camera start successed!`};
+}
+
+/*
+ * @description stop camera interval
+ *
+ * */
+Camera.prototype.end = function(){
+	if (!this.intervalID) return {result: false, message: 'interval is undefined'}
+	clearInterval(this.intervalID);
+	this.intervalID = undefined;
+	return {result: true}
+}
+
+module.exports = Camera;
